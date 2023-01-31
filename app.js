64ae73604f8d56951dd18799308cd5ec68c83cee
Ashwin Narayanan S <ashrockzzz2003@gmail.com>
2023-01-31 16:55:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+fetch('https://codeforces.com/api/contest.list')
+  .then(response => response.json())
+  .then(data => {
+    let html = document.querySelector(".contest-list");
+    const contests = data.result;
+    contests.forEach(contest => {
+        let HTML = `<div class="contest-item">
+                        <h1>${contest.name}</h1>
+                        <p class = "date">${Date(contest.startTimeSeconds*1000)}</p>
+                        <p class = "link-item"><a href="https://codeforces.com/contest/${contest.id}">Enter Contest</a></p>
+                        <p class = "about-item">${contest.phase == "BEFORE" ? "PENDING": contest.phase}</p>
+                    </div>`
+        html.insertAdjacentHTML("beforeend", HTML);
+    });
+  })
+  .catch(error => console.error(error))
