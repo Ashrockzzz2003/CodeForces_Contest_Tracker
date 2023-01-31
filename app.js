82f905536792e54069d8d7d4866254707ba3543a
Ashwin Narayanan S <ashrockzzz2003@gmail.com>
2023-01-31 17:07:04 +0530

    - date

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ fetch('https://codeforces.com/api/contest.list')
     let html = document.querySelector(".contest-list");
     const contests = data.result;
     contests.forEach(contest => {
+        let date = new Date(contest.startTimeSeconds*1000);
         let HTML = `<div class="contest-item">
+                        <p class = "date">${date.toDateString()} | ${date.toLocaleTimeString(undefined, {timeZone: 'Asia/Kolkata'})}</p>
                         <h3>${contest.name}</h1>
-                        <p class = "date">${Date(contest.startTimeSeconds*1000)}</p>
                         <p class = "link-item"><a href="https://codeforces.com/contest/${contest.id}">Enter Contest</a></p>
                         <p class = "about-item">${contest.phase == "BEFORE" ? "PENDING": contest.phase}</p>
                     </div>`
