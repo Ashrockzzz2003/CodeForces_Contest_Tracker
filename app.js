a67b4d80b9430bb103a73458e5a912950f4db2c4
Ashwin Narayanan S <ashrockzzz2003@gmail.com>
2023-01-31 17:16:15 +0530

    - update

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 fetch('https://codeforces.com/api/contest.list')
   .then(response => response.json())
   .then(data => {
-    let html = document.querySelector(".contest-list");
+    let html = document.querySelectorAll(".contest-list")[0];
     const contests = data.result;
     contests.forEach(contest => {
+        if (contest.phase == "FINISHED") {
+            html = document.querySelectorAll(".contest-list")[1];
+        }
         let date = new Date(contest.startTimeSeconds*1000);
         let HTML = `<div class="contest-item">
                         <p class = "date">${date.toDateString()} | ${date.toLocaleTimeString(undefined, {timeZone: 'Asia/Kolkata'})}</p>
                         <h3>${contest.name}</h1>
                         <p class = "link-item"><a href="https://codeforces.com/contest/${contest.id}">Enter Contest</a></p>
-                        <p class = "about-item">${contest.phase == "BEFORE" ? "PENDING": contest.phase}</p>
+                        <p class = "about-item">${contest.phase == "BEFORE" ? "UPCOMING": contest.phase}</p>
                     </div>`
         html.insertAdjacentHTML("beforeend", HTML);
     });
